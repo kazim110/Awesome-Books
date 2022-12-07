447d0aeecc5503e57306e5f352b47343b431c874
Yordi <yoriyordina7@gmail.com>
2022-12-07 15:46:27 +0300

    edit JS error

--- script1.js
+++ script1.js
@@ -3,7 +3,7 @@ const nameInput = document.getElementById('nameInput');
 const authorInput = document.getElementById('authorInput');
 const addBtn = document.querySelector('.add-btn');
 const storageKey = 'booksLocalData';
-const loadedBooks = JSON.parse(localStorage.getItem(storageKey));
+const loadedBooks = JSON.parse(localStorage.getItem(storageKey)) ||[];
 const bookslist = document.querySelector('.books-list');
 
 function loadBook() {
@@ -29,6 +29,8 @@ function addBook() {
   loadedBooks.push(book);
   localStorage.setItem(storageKey, JSON.stringify(loadedBooks));
   loadBook();
+  nameInput.value = '';
+  authorInput.value = '';
 }
 
 addBtn.addEventListener('click', addBook);
