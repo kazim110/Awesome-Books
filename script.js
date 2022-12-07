098c397f02059e7b369c153daca8712cc6b91028
kazim <kazim.mohammadi110@gmail.com>
2022-12-07 16:08:36 +0430

    Add load and delete function to script.js

--- script.js
+++ script.js
@@ -1,23 +1,40 @@
-const books = [
-  {
-    id: '1',
-    name: 'Harry Potter',
-    author: 'Yordi',
-  },
-  {
-    id: '1',
-    name: 'Harry Potter',
-    author: 'Yordi',
-  },
-];
 const nameInput = document.getElementById('nameInput');
 const authorInput = document.getElementById('authorInput');
-const addBtn = document.querySelector('add-btn');
+const addBtn = document.querySelector('.add-btn');
+const storageKey = 'booksLocalData';
+const loadedBooks = JSON.parse(localStorage.getItem(storageKey));
+const bookslist = document.querySelector('.books-list');
+
+function loadBook() {
+  bookslist.innerHTML = '';
+  if (loadedBooks != null) {
+    for (let i = 0; i < loadedBooks.length; i += 1) {
+      bookslist.innerHTML += `<div class="book-info">
+              <p>${loadedBooks[i].name}</p>
+              <p>${loadedBooks[i].author}</p>
+              <button class="remove-btn" id="btn-${i}" onclick="deleteBook(${i})" value="remove">Remove</button>
+              <hr>
+              </div>`;
+    }
+  }
+}
+
+window.onload = () => {
+  loadBook();
+};
 
 function addBook() {
   const book = { name: nameInput.value, author: authorInput.value };
-  books.push(book);
+  loadedBooks.push(book);
+
+  localStorage.setItem(storageKey, JSON.stringify(loadedBooks));
+  loadBook();
 }
-console.log(books);
 
 addBtn.addEventListener('click', addBook);
+
+function deleteBook(i) {
+  loadedBooks.splice(i, 1);
+  localStorage.setItem(storageKey, JSON.stringify(loadedBooks));
+  loadBook();
+}
